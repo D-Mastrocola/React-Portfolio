bc53cdc993d2479109007dd9b8b99a31a27c5c75
D-Mastrocola <mastrodomcola@gmail.com>
2024-07-01 17:14:09 -0400

    basic portfolio layout

--- app/src/components/Portfolio/Portfolio.jsx
+++ app/src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,37 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+
 let Portfolio = () => {
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+
   return (
-    <main className="main-content">
-      <h1>Portfolio</h1>
+    <main className="main-content" id='portfolio'>
+      <img className='project-image' src='./assets/images/budgetBuddy.png'/>
+      <List component="nav">
+        <ListItem
+          selected={selectedIndex === 0}
+          onClick={(event) => handleListItemClick(event, 0)}
+        >
+          Weather Dashboard
+        </ListItem>
+        <ListItem
+          selected={selectedIndex === 1}
+          onClick={(event) => handleListItemClick(event, 1)}
+        >
+          Budget Buddy
+        </ListItem>
+        <ListItem
+          selected={selectedIndex === 2}
+          onClick={(event) => handleListItemClick(event, 2)}
+        >
+          Pathfinder
+        </ListItem>
+      </List>
     </main>
   );
 };
