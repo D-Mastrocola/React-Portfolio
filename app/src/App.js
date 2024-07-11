19130ff2af11338e8536d3e98b11fa6c709c56fa
D-Mastrocola <mastrodomcola@gmail.com>
2024-07-10 22:10:54 -0400

    header style changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Landing from './components/Landing/Landing';
-import About from './components/About/About';
-import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Landing from "./components/Landing/Landing";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
@@ -19,16 +19,16 @@ function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/portfolio" element={<Portfolio />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-        </Routes>
-      </BrowserRouter>
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/portfolio" element={<Portfolio />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
-      
     </div>
   );
 }
--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -1,16 +1,26 @@
-import logo from '../../logo.svg';
+import pfp from "../../assets/images/portfolioPicture.jpg";
+import List from "@mui/material/List";
+import { ListItem, ListItemButton, ListItemText } from "@mui/material";
 
 let Header = () => {
-    return(
-        <header>
-            <img id='header-logo' src={logo} alt="Logo"/>
-            <ul>
-                <li>About</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-                <li>Resume</li>
-            </ul>
-        </header>
-    )
-}
+  return (
+    <header>
+      <img id="header-logo" src={pfp} alt="Logo" />
+      <List>
+        <ListItemButton component="a" href="#about">
+          <ListItemText primary="About" />
+        </ListItemButton>
+        <ListItemButton component="a" href="#portfolio">
+          <ListItemText primary="Portfolio" />
+        </ListItemButton>
+        <ListItemButton component="a" href="#contact">
+          <ListItemText primary="Contact" />
+        </ListItemButton>
+        <ListItemButton component="a" href="#resume">
+          <ListItemText primary="Resume" />
+        </ListItemButton>
+      </List>
+    </header>
+  );
+};
 export default Header;
