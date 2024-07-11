0b231b391486bc513c7998c468a9d4361f63e20e
D-Mastrocola <mastrodomcola@gmail.com>
2024-07-11 12:42:26 -0400

    frameworks for style done

--- app/src/components/Contact/Contact.jsx
+++ app/src/components/Contact/Contact.jsx
@@ -1,7 +1,47 @@
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  ListItemAvatar,
+  Avatar,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FolderIcon from "@mui/icons-material/Folder";
+
 let Contact = () => {
   return (
-    <main className="main-content">
-      <h1>Contact</h1>
+    <main className="main-content" id='contact'>
+      <div>
+        <Typography variant="h4" component={"h3"}>
+          Let's Get In Touch
+        </Typography>
+        <Typography variant="body1" component={"p"}>
+          Interested in working with, hiring or talking to me? Let's talk about
+          it!
+          <br />
+          Feel free to message me.
+        </Typography>
+      </div>
+      <List>
+        {
+          <ListItem
+            secondaryAction={
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            }
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <FolderIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Single-line item" />
+          </ListItem>
+        }
+      </List>
     </main>
   );
 };
