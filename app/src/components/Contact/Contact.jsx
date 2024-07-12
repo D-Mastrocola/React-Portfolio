ed6e9cf7aeaf7ad6933319580d5e9785e709cd96
D-Mastrocola <mastrodomcola@gmail.com>
2024-07-12 14:33:08 -0400

    new style card for contact page

--- app/src/components/Contact/Contact.jsx
+++ app/src/components/Contact/Contact.jsx
@@ -1,57 +1,49 @@
 import {
+  Button,
   Card,
   CardContent,
   CardMedia,
   Typography,
   List,
   ListItem,
-  ListItemText,
-  IconButton,
-  ListItemAvatar,
-  Avatar,
+  ListItemText
 } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import FolderIcon from "@mui/icons-material/Folder";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import EmailIcon from '@mui/icons-material/Email';
 
 let Contact = () => {
   return (
     <main className="main-content" id="contact">
-      <Card>
-      <CardMedia
-        sx={{ height: 360 }}
-        image="/assets/images/portfolioPicture.jpg"
-        title="green iguana"
-      />
+      <Card sx={{ display: 'flex' }}>
+        <CardMedia
+          sx={{ width: 360 }}
+          image="/assets/images/portfolioPicture.jpg"
+          title="dominic mastrocola"
+        />
         <CardContent>
-          <Typography variant="h4" component={"h3"}>
+          <Typography textAlign='left' variant="h4" component={"h3"}>
             Let's Get In Touch
           </Typography>
-          <Typography variant="body2" component={"p"} color="text.secondary">
+          <Typography textAlign='left' style={{fontSize: '1.1em'}} variant="body2" component={"p"} color="text.secondary">
             Interested in working with, hiring or talking to me? Let's talk
             about it!
             <br />
             Feel free to message me.
           </Typography>
+          <List>
+            <ListItem>
+              <Button startIcon={<InstagramIcon />}>stoic.ginger</Button>
+            </ListItem>
+            <ListItem>
+              <Button startIcon={<LocalPhoneIcon />}>(330)-236-6774</Button>
+            </ListItem>
+            <ListItem>
+              <Button startIcon={<EmailIcon />}>mastrodomcola@gmail.com</Button>
+            </ListItem>
+          </List>
         </CardContent>
       </Card>
-      <List>
-        {
-          <ListItem
-            secondaryAction={
-              <IconButton edge="end" aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-            }
-          >
-            <ListItemAvatar>
-              <Avatar>
-                <FolderIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Single-line item" />
-          </ListItem>
-        }
-      </List>
     </main>
   );
 };
